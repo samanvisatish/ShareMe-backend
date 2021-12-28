bbce07bffd780f9637c601055b4350113073f05f
samanvisatish <samanvissatish@gmail.com>
2021-12-28 21:42:32 +0530

    share me

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
 // Cors 
-
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
 
 // Default configuration looks like
 // {
@@ -15,6 +18,7 @@ const cors = require('cors');
 //     "optionsSuccessStatus": 204
 //   }
 
+app.use(cors(corsOptions))
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
